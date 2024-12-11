f87beada98bad4098f315c74d214e3891b348456
SymWizard07 <cade0915v2@gmail.com>
2024-12-10 19:06:21 -0600

    created rounds system

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import utility_aew_caw as util
 import turtle
+import random
+import time
 import sys
 sys.path.append('./modules')
 from pynput.keyboard import Key, Listener
@@ -9,21 +11,83 @@ util.setup_screen(screen)
 ori = turtle.Turtle()
 util.setup_turtle(ori)
 
+player1_fired = False
+player2_fired = False
+round_started = False
+
 def on_press(key):
+    if key == Key.space and util.has_focus(screen):
+        if not round_started:
+            #round_started = True
+            start_round()
     try:
         if key.char == 'z' and util.has_focus(screen):
-            print("Pow!")
+            player1_fired = True
+        elif key.char == 'm' and util.has_focus(screen):
+            player2_fired = True
+        # detect space key to start a new round
     except AttributeError:
         pass
 
 def on_release(key):
-    pass
+    try:
+        if key.char == 'z' and util.has_focus(screen):
+            player1_fired = False
+        elif key.char == 'm' and util.has_focus(screen):
+            player2_fired = False
+    except AttributeError:
+        pass
 
 listener = Listener(on_press=on_press, on_release=on_release)
 listener.start()
 
+player1_score = 0
+player2_score = 0
+
+def point(player):
+    if player == 1:
+        player1_score += 1
+    else:
+        player2_score += 1
+    util.draw_scores(ori, player1_score, player2_score)
+
+def start_round():
+    # display prepare text. A user who fires in this period will lose.
+    util.draw_text(ori, "Ready...")
+    start_time = time.time()
+    # randomize time until draw
+    duration = random.randint(3, 5)
+    while time.time() - start_time < duration:
+        # clear ready text after 2 seconds
+        if time.time() - start_time > 2:
+            util.clear_text(ori)
+        if player1_fired:
+            point(2)
+            util.draw_text(ori, "Player 1 shot too early!")
+            return
+        elif player2_fired:
+            point(1)
+            util.draw_text(ori, "Player 2 shot too early!")
+            return
+        print(time.time() - start_time)
+        time.sleep(0.1)
+    # display draw text. A user who fires in this period will win a point.
+    util.draw_text(ori, "Draw!")
+    while not player1_fired and not player2_fired:
+        if player1_fired:
+            point(1)
+            util.draw_text(ori, "Player 1 wins!")
+            return
+        elif player2_fired:
+            point(2)
+            util.draw_text(ori, "Player 2 wins!")
+            return
+        time.sleep(0.1)
+
+
+
 def __main__():
-    pass
+    util.draw_text(ori, "Press space to start a new round")
 
 __main__()
 
--- utility_aew_caw.py
+++ utility_aew_caw.py
@@ -20,6 +20,72 @@ def setup_turtle(ori):
     ori.speed(0)
     return ori
 
+def draw_text(ori, text):
+    clear_text(ori)
+    ori.penup()
+    ori.goto(0, 150)
+    ori.pendown()
+    ori.fillcolor("black")
+    ori.color("black")
+    ori.write(text, font=("Courier", 32, "bold"), align="center")
+
+def clear_text(ori):
+    # draw rectangle over previous text
+    rect_size = 600
+    ori.penup()
+    ori.goto(-1 * (rect_size / 2), 200)
+    ori.pendown()
+    ori.fillcolor("orange")
+    ori.color("orange")
+    ori.begin_fill()
+    for i in range(2):
+        ori.forward(rect_size)
+        ori.right(90)
+        ori.forward(50)
+        ori.right(90)
+    ori.end_fill()
+
+# draw scores for each player on the left and right side of the top of the screen.
+def draw_scores(ori, player1_score, player2_score):
+    # draw rectangle over previous text
+    rect_size = 150
+    ori.penup()
+    ori.goto(-1 * (rect_size / 2), 300)
+    ori.pendown()
+    ori.fillcolor("orange")
+    ori.color("orange")
+    ori.begin_fill()
+    for i in range(2):
+        ori.forward(rect_size)
+        ori.right(90)
+        ori.forward(50)
+        ori.right(90)
+    ori.end_fill()
+    ori.penup()
+    ori.goto(-1 * (rect_size / 2), 300)
+    ori.pendown()
+    ori.fillcolor("black")
+    ori.color("black")
+    ori.write("Player 1: " + str(player1_score), font=("Courier", 24, "bold"), align="center")
+    ori.penup()
+    ori.goto(0, 300)
+    ori.pendown()
+    ori.fillcolor("orange")
+    ori.color("orange")
+    ori.begin_fill()
+    for i in range(2):
+        ori.forward(rect_size)
+        ori.right(90)
+        ori.forward(50)
+        ori.right(90)
+    ori.end_fill()
+    ori.penup()
+    ori.goto(0, 300)
+    ori.pendown()
+    ori.fillcolor("black")
+    ori.color("black")
+    ori.write("Player 2: " + str(player2_score), font=("Courier", 24, "bold"), align="center")
+
 def done():
     turtle.done()
 
